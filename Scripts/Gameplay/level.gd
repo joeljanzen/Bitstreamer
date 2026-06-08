b082d9fa068ae9b52875e2f1d332463c00ecc405
Joel Janzen <joeljanzen07@gmail.com>
2026-06-07 23:06:45 -0500

    Working on some levels

--- Scripts/Gameplay/level.gd
+++ Scripts/Gameplay/level.gd
@@ -57,7 +57,7 @@ func _ready() -> void:
 	_play_area.no_bits_left.connect(_completed)
 	_levelUI.set_UI_visible(GameSettings.level_UI_enabled)
 
-	if load_level("tutorial"):
+	if load_level("memecore"):
 		print("Successfully loaded level: %s" % level_name)
 		print("BPM: %s" % bpm)
 		print("Speed: %s" % bit_speed)
