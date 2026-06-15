0e6961d6e0ff770f43d0aa427e5adfaf60c78b34
Joel Janzen <joeljanzen07@gmail.com>
2026-06-14 20:58:18 -0500

    Access more settings through game settings script

--- Scripts/Global/game_settings.gd
+++ Scripts/Global/game_settings.gd
@@ -49,16 +49,46 @@ var missed_click_colour := "FFFFFF"
 var incorrect_click_colour := "C21515"
 
 
+## Sets the default values that are set with functions (these values are not stored
+## here)
+func _ready() -> void:
+	set_bit_click_effect(true)
+	set_effect_ignores_perfect_clicks(false)
+	set_bit_fade_effect(false)
+	set_bit_miss_effect(Bit.MissEffectType.MOVE_OFFSCREEN)
+
+
 ## Connect the Flowerwall CRT script when it is loaded.
-func connect_crt_shader(crt_shader: flowerwallCRT):
+func connect_crt_shader(crt_shader: flowerwallCRT) -> void:
 	_crt_shader = crt_shader
 	if !_crt_effect:
 		_crt_shader.disable_shader()
 
 
 ## Set the CRT effect on or off.
-func set_crt_effect(enabled: bool):
+func set_crt_effect(enabled: bool) -> void:
 	if enabled and !_crt_shader.is_enabled:
 		_crt_shader.enable_shader()
 	elif _crt_shader.is_enabled:
 		_crt_shader.disable_shader()
+
+
+## Set the effect indicating the acccuracy of the bit click (or miss) on or off.
+func set_bit_click_effect(enabled: bool) -> void:
+	Bit.bit_click_effect = enabled
+
+
+## Set ignoring perfect clicks with the bit click effect on or off.
+func set_effect_ignores_perfect_clicks(enabled: bool) -> void:
+	BitClickEffect.ignores_perfect_clicks = enabled
+
+
+## Set the effect that fades the bit away on or off.
+func set_bit_fade_effect(enabled: bool) -> void:
+	Bit.bit_fade_effect = enabled
+
+
+## Set the effect that plays when a bit is missed on or off.
+## FADE_OUT does the same as DISAPPEAR unless the bit fade effect is enabled.
+func set_bit_miss_effect(effect: Bit.MissEffectType) -> void:
+	Bit.miss_effect = effect
