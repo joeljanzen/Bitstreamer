ee993f046581f87e47bb493040faa28f3ffa2bb5
Joel Janzen <joeljanzen07@gmail.com>
2026-06-25 18:55:16 -0500

    Level button text display tweaks

--- Scripts/UI/level_button.gd
+++ Scripts/UI/level_button.gd
@@ -16,8 +16,9 @@ signal button_focused
 var level_info: LevelInfo
 
 
-## Initialize the level button, given all level details.
-func _init(level_information: LevelInfo) -> void:
+## Setup the level button with all level details. Call this after instantiation 
+## of the scene but before adding as a child to the current scene tree.
+func setup(level_information: LevelInfo) -> void:
 	level_info = level_information
 
 
@@ -25,18 +26,27 @@ func _init(level_information: LevelInfo) -> void:
 func _ready() -> void:
 	_name_label.text = level_info.level_name
 	_length_label.text = _float_as_time(level_info.length)
-	_bpm_label.text = str(level_info.bpm) + " BPM"
-	_difficulty_label.text = "Difficulty: " + str(level_info.difficulty)
-	_speed_label = "Speed: " + str(level_info.speed)
-	_damage_label = "Damage: " + str(level_info.damage)
+	_bpm_label.text = _trim_decimals(level_info.bpm) + " BPM"
+	_difficulty_label.text = "Difficulty: " + _trim_decimals(level_info.difficulty)
+	_speed_label.text = "Speed: " + _trim_decimals(level_info.speed)
+	_damage_label.text = "Damage: " + str(level_info.damage)
 
 
 ## Converts the level length in seconds to a string in minutes and seconds.
 func _float_as_time(level_length: float) -> String:
 	var total_seconds = round(level_length)
-	var minutes = floor(level_length / 60.0)
-	var seconds = total_seconds - (minutes * 60)
+	var minutes: int = floor(level_length / 60.0)
+	var seconds: int = total_seconds - (minutes * 60)
 	return str(minutes) + "m " + str(seconds) + "s"
+
+
+## If the float has decimal places, it rounds to 2 places. Otherwise, it 
+## includes no decimal places and treats it like an int.
+func _trim_decimals(value: float) -> String:
+	if fmod(value, 1.0) == 0.0: # The float is really just an int.
+		return str(int(value))
+	else:
+		return str(snappedf(value, 0.01))
 
 
 func _on_play_button_pressed() -> void:
