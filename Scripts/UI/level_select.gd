ca75a67ad82206799d82ffb8080c786ee3257221
Joel Janzen <joeljanzen07@gmail.com>
2026-07-05 13:58:08 -0500

    Filter buttons disable when selected

--- Scripts/UI/level_select.gd
+++ Scripts/UI/level_select.gd
@@ -4,16 +4,23 @@ extends Control
 @onready var _back_button = $CanvasLayer/BackButton
 @onready var _scroll_box = $CanvasLayer/HBoxContainer/ScrollContainer
 @onready var _filters_panel = $CanvasLayer/FiltersPanel
-@onready var _filters_button = $CanvasLayer/FiltersButton
+@onready var _filters_button_toggle = $CanvasLayer/FiltersButton
+@onready var _filter_button_container = $CanvasLayer/FiltersPanel/MarginContainer/FilterButtonContainer
 
 @onready var _menu_focus_sound: AudioStreamPlayer = $MenuFocus
 @onready var _menu_click_sound: AudioStreamPlayer = $MenuClick
+
+## All the buttons to choose a filter for level sorting.
+var _filter_buttons: Array[Button]
 
 ## Emitted when the close button is pressed, or esc is pressed.
 signal selection_closed
 
 ## The last comparator function used to sort levels.
 static var _last_filter_used: Callable = _compare_level_difficulty
+
+## The filter button that is currently selected.
+static var _current_filter_index: int = 1
 
 ## The last position the player was on the level selection menu (on the 
 ## scrollbar).
@@ -28,6 +35,13 @@ var _levels: Array[LevelInfo]
 ## Load all levels and create buttons for them.
 func _ready() -> void:
 	_scroll_box.set_deferred("scroll_horizontal", _last_level_select_position)
+	
+	# Add filter button children
+	for child in _filter_button_container.get_children():
+		if child is Button:
+			_filter_buttons.push_back(child)
+	
+	_filter_buttons[_current_filter_index].disabled = true
 	
 	# Load all level info
 	var level_filenames: PackedStringArray = DirAccess.get_files_at("res://Levels")
@@ -88,7 +102,7 @@ func _level_button_focused() -> void:
 ## Hides the level select UI.
 func hide_UI():
 	_back_button.hide()
-	_filters_button.hide()
+	_filters_button_toggle.hide()
 	_level_button_container.hide()
 	
 	_scroll_box.mouse_filter = _scroll_box.MOUSE_FILTER_IGNORE
@@ -98,7 +112,7 @@ func hide_UI():
 ## Shows the level select UI.
 func show_UI():
 	_back_button.show()
-	_filters_button.show()
+	_filters_button_toggle.show()
 	_level_button_container.show()
 	_scroll_box.mouse_filter = _scroll_box.MOUSE_FILTER_STOP
 	
@@ -131,13 +145,13 @@ func _on_back_button_mouse_entered() -> void:
 func _on_filters_button_mouse_entered() -> void:
 	_menu_focus_sound.play()
 	_filters_panel.show()
-	_filters_button.hide()
+	_filters_button_toggle.hide()
 
 
 ## Close the filters panel.
 func _on_filters_panel_mouse_exited() -> void:
 	_filters_panel.hide()
-	_filters_button.show()
+	_filters_button_toggle.show()
 
 # Various comparator functions, for sorting levels by various properties.
 
@@ -175,28 +189,51 @@ static func _compare_level_bit_count(a: LevelButton, b: LevelButton) -> bool:
 func _on_name_pressed() -> void:
 	_menu_click_sound.play()
 	_sort_levels_by_comparator(_compare_level_name)
+	
+	_filter_buttons[_current_filter_index].disabled = false
+	_current_filter_index = 0
+	_filter_buttons[_current_filter_index].disabled = true
 
 
 func _on_difficulty_pressed() -> void:
 	_menu_click_sound.play()
 	_sort_levels_by_comparator(_compare_level_difficulty)
+	
+	_filter_buttons[_current_filter_index].disabled = false
+	_current_filter_index = 1
+	_filter_buttons[_current_filter_index].disabled = true
 
 
 func _on_speed_pressed() -> void:
 	_menu_click_sound.play()
 	_sort_levels_by_comparator(_compare_level_speed)
+	
+	_filter_buttons[_current_filter_index].disabled = false
+	_current_filter_index = 2
+	_filter_buttons[_current_filter_index].disabled = true
 
 
 func _on_damage_pressed() -> void:
 	_menu_click_sound.play()
 	_sort_levels_by_comparator(_compare_level_damage)
+	
+	_filter_buttons[_current_filter_index].disabled = false
+	_current_filter_index = 3
+	_filter_buttons[_current_filter_index].disabled = true
 
 
 func _on_length_pressed() -> void:
 	_menu_click_sound.play()
 	_sort_levels_by_comparator(_compare_level_length)
+	_filter_buttons[_current_filter_index].disabled = false
+	_current_filter_index = 4
+	_filter_buttons[_current_filter_index].disabled = true
 
 
 func _on_bit_count_pressed() -> void:
 	_menu_click_sound.play()
 	_sort_levels_by_comparator(_compare_level_bit_count)
+	
+	_filter_buttons[_current_filter_index].disabled = false
+	_current_filter_index = 5
+	_filter_buttons[_current_filter_index].disabled = true
